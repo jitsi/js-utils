9ebe373c5e8b1ae7601e7a391ef7aeeba61c94e3
paweldomas <pawel.domas@jitsi.org>
2018-07-30 10:09:31 -0500

    ref(browser-detection/index): do not use export * as
    A workaround to not use 'export * as' which is not understandable to
    the Metro Bundler used in the React-Native.

--- browser-detection/index.js
+++ browser-detection/index.js
@@ -1,2 +1,7 @@
 export { default as BrowserDetection } from './BrowserDetection';
-export * as browsers from './browsers';
+
+
+// XXX This is a workaround to not use 'export * as' which is not
+// understandable to the Metro Bundler used in the React-Native.
+import * as browsers from './browsers';
+export { browsers };
