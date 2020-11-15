91128d398988ee04fb11f1765b1f3c9c35adc1e6
Saúl Ibarra Corretgé <s@saghul.net>
2020-11-15 21:57:55 +0100

    fix(avatar) remove no longer needed code
    We haven't used this function in a long time.

--- avatar/index.js
+++ avatar/index.js
@@ -1,36 +1,6 @@
 import md5 from 'js-md5';
 
 /**
- * Returns the Avatar URL to be used for the participant.
- *
- * @param {string} [participant.avatarID] - Participant's avatar ID.
- * @param {string} [participant.email] - Participant's e-mail address.
- * @param {string} [participant.id] - Participant's ID.
- * @param {string} [avatarService.urlPrefix] - URL Prefix of the avatar service.
- * @param {string} [avatarService.urlSuffix] - URL Suffix of the avatar service.
- * @returns {string} - Avatar URL.
- */
-export function getAvatarURL(participant, avatarService = {}) {
-    const { avatarID, email, id } = participant;
-    const { urlPrefix = 'https://abotars.jitsi.net/meeple/', urlSuffix = '' } = avatarService;
-
-    return getGravatarURL(email)
-        || generateAvatarURL(avatarID || id, urlPrefix, urlSuffix);
-}
-
-/**
- * Returns the Avatar URL generated from the given avatar service.
- *
- * @param {string} key - Key using which avatar has to be generated.
- * @param {string} urlPrefix - URL Prefix of the avatar service to be used.
- * @param {string} urlSuffix - URL Suffix of the avatar service to be used.
- * @returns {string}
- */
-function generateAvatarURL(key, urlPrefix, urlSuffix) {
-    return urlPrefix + md5.hex(key.trim().toLowerCase()) + urlSuffix;
-}
-
-/**
  * Returns the Gravatar URL of a given email id.
  *
  * @param {string} key - Email or id for which we need gravatar URL.
