c2e42e1a7faac949f7432e0eef3f7958ada54149
Saúl Ibarra Corretgé <s@saghul.net>
2020-11-15 21:54:46 +0100

    feat(avatar) make Gravatar URL configurable, default to libravatar
    libravatar will anonymously proxy requests to Gravatar, and it's API
    compatible.

--- avatar/index.js
+++ avatar/index.js
@@ -33,17 +33,17 @@ function generateAvatarURL(key, urlPrefix, urlSuffix) {
 /**
  * Returns the Gravatar URL of a given email id.
  *
- * @param {string} key - Email or id for which we need gravatar url.
+ * @param {string} key - Email or id for which we need gravatar URL.
+ * @param {string} baseURL - Base Gravatar URL.
  * @returns {string} - Gravatar URL.
  */
-export function getGravatarURL(key) {
-    const urlPrefix = 'https://www.gravatar.com/avatar/';
+export function getGravatarURL(key, baseURL = 'https://seccdn.libravatar.org/avatar/') {
     const urlSuffix = '?d=404&size=200';
 
     // If the key is a valid email, we hash it. If it's not, we assume it's already a hashed format
     const avatarKey = isValidEmail(key) ? md5.hex(key.trim().toLowerCase()) : key;
 
-    return `${urlPrefix}${avatarKey}${urlSuffix}`;
+    return `${baseURL}${avatarKey}${urlSuffix}`;
 }
 
 /**
