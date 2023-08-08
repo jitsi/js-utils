3b74422ca05581f44e6d0761d0f2c50cd8b7523c
Saúl Ibarra Corretgé <s@saghul.net>
2023-08-08 12:49:39 +0200

    fix(ci) update action versions

--- json.js
+++ json.js
@@ -1,11 +1,18 @@
 import Bourne from '@hapi/bourne';
 
 /**
- * Safely parse JSON payloads.
+ * Safely parse JSON payloads. Makes sure the return value it's either an object
+ * or an error.
  *
  * @param {string} data - The payload to be parsed.
  * @returns The parsed object.
  */
 export function safeJsonParse(data) {
-    return Bourne.parse(data);
+    const json = Bourne.parse(data);
+
+    if (json && typeof json === 'object') {
+        return json;
+    }
+
+    return {};
 }
