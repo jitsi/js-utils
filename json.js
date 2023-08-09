d681530d9a5291ba396f2b908c2f014db6fa6172
Saúl Ibarra Corretgé <s@saghul.net>
2023-08-09 09:36:58 +0200

    fix(json) don't skip primitives when parsing JSON
    Some of our code does rely on parsing things like `false`.

--- json.js
+++ json.js
@@ -1,18 +1,11 @@
 import Bourne from '@hapi/bourne';
 
 /**
- * Safely parse JSON payloads. Makes sure the return value it's either an object
- * or an error.
+ * Safely parse JSON payloads.
  *
  * @param {string} data - The payload to be parsed.
  * @returns The parsed object.
  */
 export function safeJsonParse(data) {
-    const json = Bourne.parse(data);
-
-    if (json && typeof json === 'object') {
-        return json;
-    }
-
-    return {};
+    return Bourne.parse(data);
 }
