8d50cec36aa8b9f0df2748b47fa10b398b8b1786
Hristo Terezov <hristo@jitsi.org>
2025-11-03 10:33:51 -0600

    feat(exports): Export random and transport modules from main entry point
    All utility modules should be consistently accessible from the main
    package entry point to provide a unified API experience. Previously,
    random and transport were only available via subpath imports, creating
    inconsistency in how modules are accessed.

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ export * from './browser-detection';
 export * from './jitsi-local-storage';
 export * from './json';
 export * from './polyfills';
+export * from './random';
+export * from './transport';
